99ffee0b19e4876e44c340de0e0ee0cc4add44cb
Viet Tu Dao <48443261+DavidDaoTu@users.noreply.github.com>
2022-07-12 11:01:12 +0700

    Adding timers for clearing direction arrows

--- multiprotocol_micriumos/interface.c
+++ multiprotocol_micriumos/interface.c
@@ -1,11 +1,30 @@
+#include <stdio.h>
 #include "interface.h"
 #include "sl_simple_led_instances.h"
 #include "mp-ui.h"
+#include "sl_simple_timer.h"
+#include "app_bluetooth.h"
+
+#define CLEAR_DIRECTION_DELAY_MSEC        500
 
 static uint8_t led_state = 0;
 static interface_light_trigger_src_t led_trigger_source = interface_light_trigger_src_button;
 static interface_mac_t mac_trigger = {0};
 static interface_mac_t own_mac = {0};
+
+// Timer for clearing direction arrows
+static sl_sleeptimer_timer_handle_t clear_direction_timer;
+static uint32_t clear_direction_delay_ticks = 0;
+static void clear_direction_timer_callback (sl_sleeptimer_timer_handle_t *handle,
+                                            void *data)
+{
+  (void)handle;
+  (void)data;
+
+  mpUIClearDirection(MP_UI_DIRECTION_PROT1);
+  mpUIClearDirection(MP_UI_DIRECTION_PROT2);
+}
+
 
 static void apply_light_change(interface_light_trigger_src_t trigger,
                                interface_mac_t *mac,
@@ -28,15 +47,14 @@ void interface_light_on (interface_light_trigger_src_t trigger,
   sl_led_turn_on(&sl_led_led0);
   sl_led_turn_on(&sl_led_led1);
   
-  
-  apply_light_change(trigger, mac, 1);  
+  apply_light_change(trigger, mac, 1);
 }
 
 void interface_light_set_state (interface_light_trigger_src_t trigger,
                                 interface_mac_t *mac,
                                 uint8_t new_led_state)
 {
-  if (new_led_state != 0) {
+  if (new_led_state!= 0) {
     interface_light_on(trigger, mac);
   } else {
     interface_light_off(trigger, mac);
@@ -96,20 +114,39 @@ static void apply_light_change (interface_light_trigger_src_t trigger,
                                 interface_mac_t *mac,
                                 uint8_t new_state)
 {
+  sl_status_t status;
+
   // Update the LCD
   mpUIDisplayLight(new_state);
   
-  // The timer was running, a direction needs to be cleared !
-  if (led_trigger_source == interface_light_trigger_src_wifi) {
-    mpUIClearDirection(MP_UI_DIRECTION_PROT1);
-  } else if (led_trigger_source == interface_light_trigger_src_bluetooth) {
-    mpUIClearDirection(MP_UI_DIRECTION_PROT2);
-  }  
+  status = sl_sleeptimer_stop_timer(&clear_direction_timer);
+  if (status == SL_STATUS_OK) {
+    // The timer was running, a direction needs to be cleared !
+    if (led_trigger_source == interface_light_trigger_src_wifi) {
+      mpUIClearDirection(MP_UI_DIRECTION_PROT1);
+    } else if (led_trigger_source == interface_light_trigger_src_bluetooth) {
+      mpUIClearDirection(MP_UI_DIRECTION_PROT2);
+    }
+  }
 
   if (trigger == interface_light_trigger_src_wifi) {
     mpUIDisplayDirection(MP_UI_DIRECTION_PROT1);
+    // Start a timer which will clear the direction after a while
+    sl_sleeptimer_start_timer(&clear_direction_timer,
+                              clear_direction_delay_ticks,
+                              clear_direction_timer_callback,
+                              NULL,
+                              0,
+                              0);
   } else if (trigger == interface_light_trigger_src_bluetooth) {
     mpUIDisplayDirection(MP_UI_DIRECTION_PROT2);
+    // Start a timer which will clear the direction after a while
+    sl_sleeptimer_start_timer(&clear_direction_timer,
+                             clear_direction_delay_ticks,
+                             clear_direction_timer_callback,
+                             NULL,
+                             0,
+                             0);
   }
 
   // Send a BLE indication to update the application display
@@ -126,7 +163,14 @@ static void apply_light_change (interface_light_trigger_src_t trigger,
   }
 }
 
-void interface_init(void){
+void interface_init(void)
+{
+  // Timer for clearing direction arrows on LCD
+  sl_sleeptimer_init();
+
+  // Compute once for all the delay to clear the direction on the LCD
+  clear_direction_delay_ticks = ((uint64_t)CLEAR_DIRECTION_DELAY_MSEC *
+                                  sl_sleeptimer_get_timer_frequency()) / 1000;
   mpUIInit();
   mpUIClearMainScreen((uint8_t *)"Multiprotocol", true, true);
 }
